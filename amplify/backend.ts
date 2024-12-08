34aef06227e794d057c20ec551e66b0c43765fef
HillTopTRPG <mihikari90@gmail.com>
2024-12-08 11:37:08 +0900

    パスワードポリシーを変更

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,21 @@ import { defineBackend } from '@aws-amplify/backend'
 import { auth } from './auth/resource'
 import { data } from './data/resource'
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 })
+
+const { cfnUserPool } = backend.auth.resources.cfnResources
+
+// パスワードポリシーを変更
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 8,
+    requireLowercase: true,
+    requireNumbers: true,
+    requireUppercase: true,
+    requireSymbols: false,
+    temporaryPasswordValidityDays: 3,
+  },
+}
