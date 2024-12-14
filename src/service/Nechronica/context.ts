96efff2cd87ebaca082469dbe51d951427b1e89e
HillTopTRPG <mihikari90@gmail.com>
2024-12-14 11:56:30 +0900

    Add modelの認証方法の調整

--- src/service/Nechronica/context.ts
+++ src/service/Nechronica/context.ts
@@ -9,26 +9,13 @@ const client = generateClient<Schema>()
 
 const useNechronica = () => {
   const [loading, setLoading] = useState<boolean>(true)
-  const [characters, setCharacter] = useState<
-    Schema['NechronicaCharacter']['type'][]
-  >([])
 
-  const [todos, setTodos] = useState<Schema['Todo']['type'][]>([])
+  type Todo = Schema['Todo']['type']
+  const [todos, setTodos] = useState<Todo[]>([])
   useEffect(() => {
     const sub = client.models.Todo.observeQuery().subscribe({
       next: ({ items }) => {
         setTodos([...items])
-        setLoading(false)
-      },
-    })
-
-    return () => sub.unsubscribe()
-  }, [])
-  useEffect(() => {
-    const sub = client.models.NechronicaCharacter.observeQuery().subscribe({
-      next: ({ items }) => {
-        console.log('$$$$$$$$$')
-        setCharacter([...items])
         setLoading(false)
       },
     })
@@ -44,6 +31,53 @@ const useNechronica = () => {
     client.models.Todo.delete({ id })
   }
 
+  type Todo2 = Schema['Todo2']['type']
+  const [todo2s, setTodos2] = useState<Todo2[]>([])
+  useEffect(() => {
+    const sub = client.models.Todo2.observeQuery().subscribe({
+      next: ({ items }) => {
+        setTodos2([...items])
+        setLoading(false)
+      },
+    })
+    return () => sub.unsubscribe()
+  }, [])
+  const createTodo2 = (todo: Schema['Todo2']['createType']) => {
+    client.models.Todo2.create(todo)
+  }
+  const updateTodo2 = (todo: Schema['Todo2']['updateType']) => {
+    client.models.Todo2.update(todo)
+  }
+  const deleteTodo2 = (id: string) => {
+    client.models.Todo2.delete({ id })
+  }
+
+  type NechronicaCharacter = Schema['NechronicaCharacter']['type']
+  const [characters, setCharacter] = useState<NechronicaCharacter[]>([])
+  useEffect(() => {
+    const sub = client.models.NechronicaCharacter.observeQuery().subscribe({
+      next: ({ items }) => {
+        console.log('$$$$$$$$$')
+        setCharacter([...items])
+        setLoading(false)
+      },
+    })
+    return () => sub.unsubscribe()
+  }, [])
+  const createCharacter = (
+    sheetId: string,
+    type: 'doll' | 'savant' | 'horror' | 'legion',
+    data: Nechronica,
+  ) => {
+    console.log('regist!!!')
+    client.models.NechronicaCharacter.create({
+      name: data.basic.characterName,
+      type,
+      sheetId,
+      sheetData: JSON.stringify(data),
+    })
+  }
+
   return {
     loading,
     todoCrud: {
@@ -52,28 +86,28 @@ const useNechronica = () => {
       updateTodo,
       deleteTodo,
     },
+    todo2Crud: {
+      todo2s,
+      createTodo2,
+      updateTodo2,
+      deleteTodo2,
+    },
     dolls: characters.filter((c) => c.type === 'doll'),
     savansts: characters.filter((c) => c.type === 'savant'),
     horrors: characters.filter((c) => c.type === 'horror'),
     legions: characters.filter((c) => c.type === 'legion'),
-    createCharacter: (
-      sheetId: string,
-      type: 'doll' | 'savant' | 'horror' | 'legion',
-      data: Nechronica,
-    ) => {
-      console.log('regist!!!')
-      client.models.NechronicaCharacter.create({
-        name: data.basic.characterName,
-        type,
-        sheetId,
-        data,
-      })
-    },
+    createCharacter,
   }
 }
 
-export const [NechronicaProvider, useNechronicaContext, useTodoCrud] = constate(
+export const [
+  NechronicaProvider,
+  useNechronicaContext,
+  useTodoCrud,
+  useTodo2Crud,
+] = constate(
   useNechronica,
-  (v) => omit(v, 'todoCrud'),
+  (v) => omit(v, 'todoCrud', 'todo2Crud'),
   (v) => v.todoCrud,
+  (v) => v.todo2Crud,
 )
