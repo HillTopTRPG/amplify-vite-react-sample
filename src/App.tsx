79574b1144d9366b5b24cdb47122b00901e58091
HillTopTRPG <mihikari90@gmail.com>
2024-12-29 23:09:59 +0900

    Change test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from 'react-router-dom'
 import PrivateLayout from '@/components/PrivateLayout.tsx'
 import PublicLayout from '@/components/PublicLayout.tsx'
 import Home from '@/pages/Home.tsx'
 import NotFound from '@/pages/NotFound.tsx'
 import nechronicaRoutes from '@/service/Nechronica/Routes.tsx'
 
+function Root() {
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  )
+}
+
 const routes = createBrowserRouter([
   {
-    path: '/nechronica',
-    Component: Home,
-  },
-  {
-    element: <PublicLayout />,
-    children: [...nechronicaRoutes.public],
-  },
-  {
-    element: <PrivateLayout />,
-    children: [...nechronicaRoutes.private],
-  },
-  {
-    path: '*',
-    Component: NotFound,
+    path: '/',
+    Component: Root,
+    children: [
+      {
+        path: '/',
+        Component: Home,
+      },
+      {
+        element: <PublicLayout />,
+        children: [...nechronicaRoutes.public],
+      },
+      {
+        element: <PrivateLayout />,
+        children: [...nechronicaRoutes.private],
+      },
+      {
+        path: '*',
+        Component: NotFound,
+      },
+    ],
   },
 ])
 
