835ae7934905fdaece0d322c9a47dd47738257dd
HillTopTRPG <mihikari90@gmail.com>
2025-01-19 12:36:02 +0900

    Change マニューバ画面からビルド画面にマニューバをコピーできるようにした

--- src/service/Nechronica/components/DetailSider/ManeuverDetailSider/ManeuverImportButton.tsx
+++ src/service/Nechronica/components/DetailSider/ManeuverDetailSider/ManeuverImportButton.tsx
@@ -1,11 +1,40 @@
+import { useCallback, useMemo } from 'react'
 import { BuildOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
+import { Button, message } from 'antd'
+import { clone } from 'lodash-es'
+import { useCharacterMakeContext } from '@/service/Nechronica/components/BuildContents/context.ts'
+import { useNechronicaContext } from '@/service/Nechronica/context.ts'
 
 export default function ManeuverImportButton() {
-  return (
-    <Button style={{ marginLeft: 8, marginTop: 8 }}>
-      <BuildOutlined />
-      選択マニューバをビルド画面に取り込む
-    </Button>
+  const { selectedManeuverInfos, setSelectedManeuverInfos } =
+    useNechronicaContext()
+  const { setManeuvers } = useCharacterMakeContext()
+  const [messageApi, contextHolder] = message.useMessage()
+
+  const onClick = useCallback(() => {
+    setManeuvers((prev) => [
+      ...prev,
+      ...selectedManeuverInfos.map((info) => clone(info.maneuver)),
+    ])
+    messageApi
+      .info(`${selectedManeuverInfos.length}個のマニューバのコピー完了`)
+      .then()
+    setSelectedManeuverInfos([])
+  }, [
+    messageApi,
+    selectedManeuverInfos,
+    setManeuvers,
+    setSelectedManeuverInfos,
+  ])
+
+  return useMemo(
+    () => (
+      <Button style={{ marginLeft: 8, marginTop: 8 }} onClick={onClick}>
+        {contextHolder}
+        <BuildOutlined />
+        選択マニューバをビルド画面にコピー
+      </Button>
+    ),
+    [contextHolder, onClick],
   )
 }
--- src/service/Nechronica/components/DetailSider/ManeuverDetailSider/index.tsx
+++ src/service/Nechronica/components/DetailSider/ManeuverDetailSider/index.tsx
@@ -1,8 +1,18 @@
+import { type CSSProperties } from 'react'
 import { Flex } from 'antd'
 import ManeuverImportButton from './ManeuverImportButton.tsx'
 import SelectedManeuversElm from './SelectedManeuversElm.tsx'
 import { useScreenContext } from '@/context/screenContext.ts'
 import { useNechronicaContext } from '@/service/Nechronica/context.ts'
+
+const CONTAINER_STYLE: CSSProperties = {
+  position: 'fixed',
+  right: 0,
+  top: 48,
+  bottom: 0,
+  width: 336,
+  overflow: 'auto',
+}
 
 export default function ManeuverDetailSider() {
   const { screenSize } = useScreenContext()
@@ -19,8 +29,8 @@ export default function ManeuverDetailSider() {
       >
         <ManeuverImportButton />
         <Flex
-          gap={8}
           align="flex-start"
+          gap={8}
           justify="space-evenly"
           wrap
           style={{ alignContent: 'flex-start' }}
@@ -33,14 +43,7 @@ export default function ManeuverDetailSider() {
 
   return (
     <div
-      style={{
-        position: 'fixed',
-        right: 0,
-        top: 48,
-        bottom: 0,
-        width: 336,
-        overflow: 'auto',
-      }}
+      style={CONTAINER_STYLE}
       onScrollCapture={(e) => {
         e.stopPropagation()
         setHoverManeuverId('')
